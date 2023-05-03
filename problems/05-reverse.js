85536431f6e65e59b769f7d72f3ac4a1b18851bb
Jayce Avery <Jred032@yahoo.com>
2023-05-03 19:12:15 -0400

    completed question one

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -14,9 +14,23 @@ reverse(""); // ""
 
 
 function reverse(str) {
-  // Your code here
+  if (str.length === 0) {
+    return ""
+  }
+  let letters = str[str.length-1];
+  let variable = str.slice(0, str.length-1);
+  // console.log(variable);
+  // console.log(letters);
+  return letters + reverse(variable);
 }
 
+// console.log(reverse("house")); // "esuoh"
+// reverse("house"); // "esuoh"
+// reverse("dog"); // "god"
+// reverse("atom"); // "mota"
+// reverse("q"); // "q"
+// reverse("id"); // "di"
+// reverse(""); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
